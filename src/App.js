05ae273ed7f73c9ce8a7a8e6730981f78509b4a2
Sandeep Sharma <sks34246@gmail.com>
2022-08-26 11:31:16 +0530

    Pehla Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import Footer from './layout/Footer/Footer';
 
 import Header from './layout/Header';
 import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App" style={{border:"3px solid black"}}>
-      <Header/>
-      <div className="App container-lg" style={{border:"3px solid red"}}>
-        <Home/>
-      </div>
-    </div>
-  );
+	return (
+		<div className='App' style={{ border: '3px solid black' }}>
+			<Header />
+			<div className='App container-lg' style={{ border: '3px solid red' }}>
+				<Home />
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
