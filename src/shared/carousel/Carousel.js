872788ee198c1d5b5e33b03ca2d4d3c1dee7453d
Sandeep Sharma <sks34246@gmail.com>
2022-09-26 17:47:36 +0530

    re-design-GETINTOUCH

--- src/shared/carousel/Carousel.js
+++ src/shared/carousel/Carousel.js
@@ -1,65 +1,65 @@
-import React from "react";
-import "./Carousel.css";
+import React from 'react';
+import './Carousel.css';
 
-import Typewriter from "typewriter-effect";
+import Typewriter from 'typewriter-effect';
 
-import img2 from "../../media/carousel-img-1.png";
-import img3 from "../../media/carousel-img-2.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownLong } from "@fortawesome/free-solid-svg-icons";
+import img2 from '../../media/carousel-img-1.png';
+import img3 from '../../media/carousel-img-2.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownLong } from '@fortawesome/free-solid-svg-icons';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Carousel = () => {
-  const onScroll = () => {
-    document
-      .getElementById("whatweoffer")
-      .scrollIntoView({ block: "start", behavior: "smooth" });
-  };
+	const onScroll = () => {
+		document
+			.getElementById('whatweoffer')
+			.scrollIntoView({ block: 'start', behavior: 'smooth' });
+	};
 
-  return (
-    <div>
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide carousel-fade mr-top-4"
-        data-bs-ride="carousel"
-        style={{
-          marginTop: "0rem",
-          position: "relative",
-          left: "calc(-50vw + 50%)",
-          width: "100vw",
-        }}
-        data-aos="zoom-out"
-      >
-        <div className="top-right-menu">
-          <ul>
-            <li>
-              <Link to="/about">About</Link>
-              <span className="vertical-bar">|</span>
-            </li>
-            <li>
-              <Link to="/academics">Academics</Link>
-              <span className="vertical-bar">|</span>
-            </li>
-            {/* <li>
+	return (
+		<div>
+			<div
+				id='carouselExampleCaptions'
+				className='carousel slide carousel-fade mr-top-4'
+				data-bs-ride='carousel'
+				style={{
+					marginTop: '0rem',
+					position: 'relative',
+					left: 'calc(-50vw + 50%)',
+					width: '100vw',
+				}}
+				data-aos='zoom-out'
+			>
+				<div className='top-right-menu'>
+					<ul>
+						<li>
+							<Link to='/about'>About</Link>
+							<span className='vertical-bar'></span>
+						</li>
+						<li>
+							<Link to='/academics'>Academics</Link>
+							<span className='vertical-bar'></span>
+						</li>
+						{/* <li>
               <Link to="/admission">Admissions</Link>
               <span className="vertical-bar">|</span>
             </li> */}
-            <li>
-              <Link to="/kidslife">Kid's Life</Link>
-              <span className="vertical-bar">|</span>
-            </li>
-            {/* <li>
+						<li>
+							<Link to='/kidslife'>Kid's Life</Link>
+							<span className='vertical-bar'></span>
+						</li>
+						{/* <li>
               <Link to="/parent-corner">Parent Corner</Link>
               <span className="vertical-bar">|</span>
             </li> */}
-            <li>
-              <Link to="/management">Management Team</Link>
-              <span className="vertical-bar">|</span>
-            </li>
-          </ul>
-        </div>
-        {/* <div className="scl-name-main-container">
+						<li>
+							<Link to='/management'>Management Team</Link>
+							<span className='vertical-bar'></span>
+						</li>
+					</ul>
+				</div>
+				{/* <div className="scl-name-main-container">
           <div className="scl-name-container">
             <h5 className="royalhills">
               <Typewriter
@@ -73,49 +73,49 @@ const Carousel = () => {
             <p className="inter-scl">Internatinal School</p>
           </div>
         </div> */}
-        <div className="scroll-container">
-          <div className="scroll" onClick={onScroll}>
-            <FontAwesomeIcon className="arrow-scroll-down" icon={faDownLong} />
-          </div>
-        </div>
-        <div className="carousel-inner">
-          {/* <div className="carousel-item active">
+				<div className='scroll-container'>
+					<div className='scroll' onClick={onScroll}>
+						<FontAwesomeIcon className='arrow-scroll-down' icon={faDownLong} />
+					</div>
+				</div>
+				<div className='carousel-inner'>
+					{/* <div className="carousel-item active">
             <img src={img1} className="d-block w-100 carousel-img" alt="..." />
           </div> */}
-          <div className="carousel-item active">
-            <img src={img2} className="d-block w-100 carousel-img" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={img2} className="d-block w-100 carousel-img" alt="..." />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <div className="carousel-btn-container">
-            <div className="carousel-btn">
-              <FontAwesomeIcon icon={faDownLong} className="rotate-prev" />
-            </div>
-          </div>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <div className="carousel-btn-container">
-            <div className="carousel-btn">
-              <FontAwesomeIcon icon={faDownLong} className="rotate-next" />
-            </div>
-          </div>
-        </button>
-      </div>
-    </div>
-  );
+					<div className='carousel-item active'>
+						<img src={img2} className='d-block w-100 carousel-img' alt='...' />
+					</div>
+					<div className='carousel-item'>
+						<img src={img3} className='d-block w-100 carousel-img' alt='...' />
+					</div>
+				</div>
+				<button
+					className='carousel-control-prev'
+					type='button'
+					data-bs-target='#carouselExampleCaptions'
+					data-bs-slide='prev'
+				>
+					<div className='carousel-btn-container'>
+						<div className='carousel-btn'>
+							<FontAwesomeIcon icon={faDownLong} className='rotate-prev' />
+						</div>
+					</div>
+				</button>
+				<button
+					className='carousel-control-next'
+					type='button'
+					data-bs-target='#carouselExampleCaptions'
+					data-bs-slide='next'
+				>
+					<div className='carousel-btn-container'>
+						<div className='carousel-btn'>
+							<FontAwesomeIcon icon={faDownLong} className='rotate-next' />
+						</div>
+					</div>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Carousel;
--- src/shared/staff/Staff.js
+++ src/shared/staff/Staff.js
@@ -1,43 +1,43 @@
-import React from "react";
-import "./Staff.css";
-import staffuser from "../../media/staffuser.jpg";
+import React from 'react';
+import './Staff.css';
+import staffuser from '../../media/staffuser.jpg';
 
 const Staff = () => {
-  return (
-    <div
-      style={{
-        backgroundColor: "#f89503",
-        padding: "1rem 0rem",
-        margin: "3rem 0rem",
-        border: "10px solid #ebeb85",
-        borderRadius: "20px",
-      }}
-    >
-      <h1 style={{ margin: "2rem 0rem", textAlign: "center", color: "white" }}>
-        Meet Our Staff
-      </h1>
-      <div className="staff-container">
-        <div className="staff">
-          <div data-aos="slide-up">
-            <img className="staff-img" src={staffuser} alt="staff" />
-          </div>
-          <p data-aos="slide-down">teacher name 1</p>
-        </div>
-        <div className="staff">
-          <div data-aos="slide-up">
-            <img className="staff-img" src={staffuser} alt="staff" />
-          </div>
-          <p>teacher name 1</p>
-        </div>
-        <div className="staff">
-          <div data-aos="slide-up">
-            <img className="staff-img" src={staffuser} alt="staff" />
-          </div>
-          <p>teacher name 1</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				backgroundColor: '#f89503',
+				padding: '1rem 0rem',
+				margin: '3rem 0rem',
+				border: '10px solid #337c87',
+				borderRadius: '20px',
+			}}
+		>
+			<h1 style={{ margin: '2rem 0rem', textAlign: 'center', color: 'white' }}>
+				Meet Our Staff
+			</h1>
+			<div className='staff-container'>
+				<div className='staff'>
+					<div data-aos='slide-up'>
+						<img className='staff-img' src={staffuser} alt='staff' />
+					</div>
+					<p data-aos='slide-down'>teacher name 1</p>
+				</div>
+				<div className='staff'>
+					<div data-aos='slide-up'>
+						<img className='staff-img' src={staffuser} alt='staff' />
+					</div>
+					<p>teacher name 1</p>
+				</div>
+				<div className='staff'>
+					<div data-aos='slide-up'>
+						<img className='staff-img' src={staffuser} alt='staff' />
+					</div>
+					<p>teacher name 1</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Staff;
