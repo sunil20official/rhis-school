2749058d66c6010b295f5138cf1a9aac14504057
Sandeep Sharma <sks34246@gmail.com>
2022-09-28 17:39:26 +0530

    youtube-section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,57 @@
-import React from "react";
+import React from 'react';
 
-import Carousel from "../shared/carousel/Carousel";
-import Testimonials from "../shared/Testimonials";
-import WhatWeOffer from "../shared/whatWeOffer/WhatWeOffer";
-import Staff from "../shared/staff/Staff";
-import CountUp from "../shared/countup/CountUp";
+import Carousel from '../shared/carousel/Carousel';
+import Testimonials from '../shared/Testimonials';
+import WhatWeOffer from '../shared/whatWeOffer/WhatWeOffer';
+import Staff from '../shared/staff/Staff';
+import CountUp from '../shared/countup/CountUp';
+import RhisYouTube from '../shared/RhisYouTube';
+import PopupModal from '../shared/PopupModal';
 
 const Home = () => {
-  return (
-    <>
-      <Carousel />
-      <div className="container-lg">
-        <WhatWeOffer />
-        <div id="story" className="story-section" data-aos="zoom-out">
-          <h1>Our Story</h1>
-          <p className="story-text">
-            <p>
-              There is a future we believe in, fostered by strong souls and
-              inventive minds. We believe the way to usher in change, is to
-              empower young minds with stellar education, thus creating the
-              enablers of tomorrow.
-            </p>
-            <p>
-              Laying its foundation stone in 2021, Royal Hills International
-              School was set up with a vision: To create leaders of tomorrow by
-              imparting education. free from all prejudices with a focus on the
-              holistic development of students.
-            </p>
-            <p>
-              RHIS offers a curriculum from Grades I onwards, mapped as per the
-              Central Board of Secondary Education, New Delhi. At RHIS, we
-              encourage our student to learn, play and create new opportunities
-              for themselves. Education is not the process of teaching by the
-              teacher, it is the process to encourage the learning process of
-              the student. Our main emphasis is on providing quality education
-              and excellence in all the fields to inspire the students to be
-              morally upright and possess qualities of integrity, truthfulness
-              and honesty. Not only this, but we also develop patriotic, social
-              and ethical values among students for developing responsive
-              citizenship to revive the past glory of Indian culture .
-            </p>
-          </p>
-        </div>
-        <Testimonials />
-        <CountUp />
-        <Staff />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Carousel />
+			<div className='container-lg'>
+				<WhatWeOffer />
+				<div id='story' className='story-section' data-aos='zoom-out'>
+					<h1>Our Story</h1>
+					<p className='story-text'>
+						<p>
+							There is a future we believe in, fostered by strong souls and inventive
+							minds. We believe the way to usher in change, is to empower young minds
+							with stellar education, thus creating the enablers of tomorrow.
+						</p>
+						<p>
+							Laying its foundation stone in 2021, Royal Hills International School was
+							set up with a vision: To create leaders of tomorrow by imparting
+							education. free from all prejudices with a focus on the holistic
+							development of students.
+						</p>
+						<p>
+							RHIS offers a curriculum from Grades I onwards, mapped as per the Central
+							Board of Secondary Education, New Delhi. At RHIS, we encourage our
+							student to learn, play and create new opportunities for themselves.
+							Education is not the process of teaching by the teacher, it is the
+							process to encourage the learning process of the student. Our main
+							emphasis is on providing quality education and excellence in all the
+							fields to inspire the students to be morally upright and possess
+							qualities of integrity, truthfulness and honesty. Not only this, but we
+							also develop patriotic, social and ethical values among students for
+							developing responsive citizenship to revive the past glory of Indian
+							culture .
+						</p>
+					</p>
+				</div>
+
+				<Testimonials />
+				<RhisYouTube />
+				<PopupModal />
+				<CountUp />
+				<Staff />
+			</div>
+		</>
+	);
 };
 
 export default Home;
---
+++ src/shared/RhisYouTube.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+import '../App.css';
+
+const RhisYouTube = () => {
+	return (
+		<div className='youtube'>
+			<div className='row'>
+				<div className='col-md-6 youtube-text'>
+					<h3
+						style={{
+							margin: '2rem auto',
+							paddingBottom: '1rem',
+							textAlign: 'center',
+							borderBottom: '3px solid #f89503',
+							width: '75%',
+						}}
+					>
+						<span>SHAPING FUTURE</span>
+					</h3>
+
+					<h6>
+						We have grown into one of India's leading group of K-12 institutions,
+						providing academic excellence to our students. This is facilitated by
+						dedicated educators who are trained to channelize their energy and
+						resources towards child-centered learning. We have also reached out
+						internationally, to establish friendship institutions all across the
+						globe.
+					</h6>
+					<button className='btn px-4 py-2 m-4'>
+						<a
+							className='text-white'
+							href='https://www.youtube.com/channel/UCDWrh2uxMcfctahqfEYGrRg'
+							target='_blank'
+						>
+							Visit Our Channel
+						</a>
+					</button>
+				</div>
+				<div className='col-md-6'>
+					<ReactPlayer
+						width='100%'
+						height='320px'
+						controls
+						url='https://youtu.be/lFsrFjXF83w'
+						onReady={() => console.log('onReady callback')}
+						onStart={() => console.log('onStart callback')}
+						onPause={() => console.log('onPause callback')}
+						onEnded={() => console.log('onEnded callback')}
+						onError={() => console.log('onError callback')}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RhisYouTube;
+
+// https://www.youtube.com/watch?v=lFsrFjXF83w&ab_channel=KIDZEE%2CRoyalHillsInternationalSchool%F0%9F%93%92%F0%9F%93%9D
+// lFsrFjXF83w&ab
+// https://youtu.be/lFsrFjXF83w
