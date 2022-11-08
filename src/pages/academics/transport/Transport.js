9b46d64df34d19a14f6c043c3ba1ec409c87ba92
usersunilkumar <imsunilkumar20@gmail.com>
2022-11-08 10:50:36 +0530

    transport page done

--- src/pages/academics/transport/Transport.js
+++ src/pages/academics/transport/Transport.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Transport.css';
+import transport from '../../../media/transport.jpg';
 
 const Transport = () => {
   return (
-    <div className='transport-page-cont'>
+    <div className="transport-page-cont">
       <h1
         style={{
           textAlign: "center",
@@ -15,8 +16,21 @@ const Transport = () => {
       >
         Transport
       </h1>
+      <div className="transport-container">
+        <div className="transport-content">
+          <p>
+            As far as we talk about the transport facilities, we ensure that our
+            school provides the utmost safety to the students. Moreover, we run
+            busses and vans for our students so that they can safely reach on
+            time.
+          </p>
+        </div>
+        <div className="transport-image">
+          <img style={{width:"100%",borderRadius:"20px"}} src={transport} alt="transport vehicles"/>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Transport
