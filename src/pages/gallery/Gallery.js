3c5709883e41d51c7819e8b254df24b564f6fcab
usersunilkumar <imsunilkumar20@gmail.com>
2022-11-09 11:51:53 +0530

    gallery done

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,22 +1,110 @@
-import React from 'react';
-import './Gallery.css';
-import img1 from '../../media/images/assembly.jpg';
-import img2 from '../../media/images/bench.jpg';
-import img3 from '../../media/images/childrens_drawing.jpg';
-import img4 from '../../media/images/flag-post.jpg';
-import img5 from '../../media/images/tour.jpg';
-import img6 from '../../media/images/sports.jpg';
-import img7 from '../../media/images/interior-5.jpg';
-import img8 from '../../media/images/image-3.jpg';
-import img9 from '../../media/images/gift.jpg';
-import img10 from '../../media/images/gift-5.jpg';
-import img11 from '../../media/images/flag.jpg';
-import img12 from '../../media/images/image-4.jpg';
-import img13 from '../../media/images/gift-3.jpg';
-import img14 from '../../media/images/image-1.jpg';
+import React from "react";
+import "./Gallery.css";
+import Lightroom from "react-lightbox-gallery";
 
+import img1 from "../../media/images/assembly.jpg";
+import img2 from "../../media/images/bench.jpg";
+import img3 from "../../media/images/childrens_drawing.jpg";
+import img4 from "../../media/images/flag-post.jpg";
+import img5 from "../../media/images/tour.jpg";
+import img6 from "../../media/images/sports.jpg";
+import img7 from "../../media/images/interior-5.jpg";
+import img8 from "../../media/images/image-3.jpg";
+import img9 from "../../media/images/gift.jpg";
+import img10 from "../../media/images/gift-5.jpg";
+import img11 from "../../media/images/flag.jpg";
+import img12 from "../../media/images/image-4.jpg";
+import img13 from "../../media/images/gift-3.jpg";
+import img14 from "../../media/images/image-1.jpg";
+import img15 from "../../media/images/gift-2.jpg"
 
 const Gallery = () => {
+  var images = [
+    {
+      src: img1,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img2,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img3,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img4,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img5,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img6,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img7,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img8,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img9,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img10,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img11,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img12,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img13,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img14,
+      desc: "",
+      sub: "",
+    },
+    {
+      src: img15,
+      desc: "",
+      sub: "",
+    },
+  ];
+  var settings = {
+    columnCount: {
+      default: 3,
+      mobile: 2,
+      tab: 2,
+    },
+    mode: "dark",
+  };
+
   return (
     <div className="gallery-cont">
       <h1
@@ -32,64 +120,10 @@ const Gallery = () => {
       </h1>
 
       <div className="gallery-grid">
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img1} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img2} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img3} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img4} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img5} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img6} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img7} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img8} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img9} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img10} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img11} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img12} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img13} alt="gallery"></img>
-        </div>
-
-        <div className="gallery-img-cont">
-          <img className="gallery-img" src={img14} alt="gallery"></img>
-        </div>
+        <Lightroom images={images} settings={settings} />
       </div>
     </div>
   );
-}
+};
 
-export default Gallery
+export default Gallery;
