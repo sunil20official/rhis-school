c99f99a26cb01e40ca0cf3017d8d2bd92cbc7d7a
usersunilkumar <imsunilkumar20@gmail.com>
2022-11-18 13:35:44 +0530

    event change

--- src/pages/media/events/Events.jsx
+++ src/pages/media/events/Events.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './Events.css';
 import event1 from '../../../media/images/events_sanskriti.jpg';
 import event2 from '../../../media/images/events_budda_drawing.jpg';
-import event3 from '../../../media/images/events_christmas.jpg';
+import event3 from '../../../media/celebration.jpg';
 import event4 from '../../../media/images/events_unknown1.jpg';
+import event5 from '../../../media/images/flag-post.jpg';
 
 const Events = () => {
   return (
@@ -47,8 +48,8 @@ const Events = () => {
               <img className="event-image" src={event3} alt="" />
             </div>
             <div className="event-details">
-              <h1 className="event-heading">Sanskriti Karyakram</h1>
-              <p className="event-date">20 November, 2021</p>
+              <h1 className="event-heading">Christmas Day</h1>
+              <p className="event-date">25 December, 2021</p>
             </div>
           </div>
           {/* fourth event card */}
@@ -57,18 +58,18 @@ const Events = () => {
               <img className="event-image" src={event4} alt="" />
             </div>
             <div className="event-details">
-              <h1 className="event-heading">Sanskriti Karyakram</h1>
-              <p className="event-date">20 November, 2021</p>
+              <h1 className="event-heading">World Environment Day</h1>
+              <p className="event-date">5 June, 2022</p>
             </div>
           </div>
           {/* fifth event card */}
           <div className="event-card">
             <div className="event-image-wrapper">
-              <img className="event-image" src={event1} alt="" />
+              <img className="event-image" src={event5} alt="" />
             </div>
             <div className="event-details">
-              <h1 className="event-heading">Sanskriti Karyakram</h1>
-              <p className="event-date">20 November, 2021</p>
+              <h1 className="event-heading">Independence Day</h1>
+              <p className="event-date">15 August, 2021</p>
             </div>
           </div>
           {/* sixth event card */}
